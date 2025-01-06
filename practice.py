0599af0ddc490e585c5e6dbefee9078951e881ef
SaiRithik24 <rksairithik24@gmail.com>
2025-01-06 12:52:39 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,14 @@
+def name(x):
+    print(x)
+name("deepak")
+
+
+
+def addition():
+    x=5;y=10
+    print("value of x is",x)
+    print("value of y is",y)
+    add=x+y
+    print(add)
+
+addition()
