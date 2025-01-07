6bf3e516e31abbb0ef408d5e66e7bb202b53925d
SaiRithik24 <rksairithik24@gmail.com>
2025-01-07 03:36:06 +0000

    ...

---
+++ AD2.py
@@ -0,0 +1,28 @@
+a = "rithik"
+b = "RITHIK"
+print(a.upper())
+print(b.lower())
+
+c = "  hello there  "
+print(c.strip())
+
+d = "rithik!!!"
+print(d.rstrip("!"))
+
+print(a.replace("rithik", "ram"))
+
+e = "hello, friend how are you"
+print(e.split())
+
+print(e.capitalize())
+
+print(a.center(50))
+
+print(a.count("i"))
+
+print(a.find("i"))
+
+f = "I am good"
+print(f.index("good"))
+
+print(f.title())
